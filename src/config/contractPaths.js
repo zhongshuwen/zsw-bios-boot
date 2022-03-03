98a6ea85b656101183dbbeda2243b73a428f0107
cf <carter@carter.at>
2022-03-03 17:42:24 +0800

    added path vars

--- src/config/contractPaths.js
+++ src/config/contractPaths.js
@@ -1,23 +1,23 @@
-const EOS_CONTRACTS_BASE_PATH = "/zswdevmain/repos/eos/contracts/contracts/"
-const EOSIO_CONTRACTS_BASE_PATH = "/zswdevmain/repos/zsw-sys-contracts/build/contracts/"
-const ZSW_CORE_CONTRACTS_BASE_PATH = "/zswdevmain/repos/zsw.core/build/contracts/"
+const ZSW_CHAIN_CONTRACTS_BASE_PATH = process.env.ZSW_CHAIN_CONTRACTS_BASE_PATH || "/zswdevmain/repos/eos/contracts/contracts/"
+const ZSWHQ_CONTRACTS_BASE_PATH = process.env.ZSWHQ_CONTRACTS_BASE_PATH || "/zswdevmain/repos/zsw-sys-contracts/build/contracts/"
+const ZSW_CORE_CONTRACTS_BASE_PATH = process.env.ZSW_CORE_CONTRACTS_BASE_PATH || "/zswdevmain/repos/zsw.core/build/contracts/"
 
 const CONTRACT_PATHS = {
   "zswhq.boot": {
-    abi: EOS_CONTRACTS_BASE_PATH+"eosio.boot/bin/eosio.boot.abi",
-    wasm: EOS_CONTRACTS_BASE_PATH+"eosio.boot/bin/eosio.boot.wasm",
+    abi: ZSW_CHAIN_CONTRACTS_BASE_PATH+"eosio.boot/bin/eosio.boot.abi",
+    wasm: ZSW_CHAIN_CONTRACTS_BASE_PATH+"eosio.boot/bin/eosio.boot.wasm",
   },
   "zswhq.token": {
-    abi: EOSIO_CONTRACTS_BASE_PATH+"eosio.token/eosio.token.abi",
-    wasm: EOSIO_CONTRACTS_BASE_PATH+"eosio.token/eosio.token.wasm",
+    abi: ZSWHQ_CONTRACTS_BASE_PATH+"eosio.token/eosio.token.abi",
+    wasm: ZSWHQ_CONTRACTS_BASE_PATH+"eosio.token/eosio.token.wasm",
   },
   "zswhq.msig": {
-    abi: EOSIO_CONTRACTS_BASE_PATH+"eosio.msig/eosio.msig.abi",
-    wasm: EOSIO_CONTRACTS_BASE_PATH+"eosio.msig/eosio.msig.wasm",
+    abi: ZSWHQ_CONTRACTS_BASE_PATH+"eosio.msig/eosio.msig.abi",
+    wasm: ZSWHQ_CONTRACTS_BASE_PATH+"eosio.msig/eosio.msig.wasm",
   },
   "zswhq.system": {
-    abi: EOSIO_CONTRACTS_BASE_PATH+"eosio.system/eosio.system.abi",
-    wasm: EOSIO_CONTRACTS_BASE_PATH+"eosio.system/eosio.system.wasm",
+    abi: ZSWHQ_CONTRACTS_BASE_PATH+"eosio.system/eosio.system.abi",
+    wasm: ZSWHQ_CONTRACTS_BASE_PATH+"eosio.system/eosio.system.wasm",
   },
 
   "zsw.perms": {
